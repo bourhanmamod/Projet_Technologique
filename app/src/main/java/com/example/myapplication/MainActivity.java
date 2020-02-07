408f48706256cd0a566704d42d5869e672a8c91a
bourhanmamod <bourhanoudine.mamod-houssen@etu.u-bordeaux.fr>
2020-02-07 14:52:20 +0100

    Gallerie

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,49 +1,74 @@
 package com.example.myapplication;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 
 
 public class MainActivity extends AppCompatActivity {
-    Bitmap bitmap, original, bit;
+    static final int REQUEST_IMAGE_CAPTURE=1;
+    Bitmap bitmap;
     ImageView image;
-    TextView text;
-    int N;
-    @SuppressLint("SetTextI18n")
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        image = findViewById(R.id.color);
+        Button button = findViewById(R.id.camera);
+        image = findViewById(R.id.Ia);
 
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.color);
-        original = bitmap.copy(bitmap.getConfig(), true);
-        bit = bitmap.copy(bitmap.getConfig(), true);
-        N = bit.getWidth() * bit.getHeight();
-        text = findViewById(R.id.textView);
-        text.setText("Size: " + N);
-        image.setImageBitmap(bit);
-
+        if(!Camera()){
+            button.setEnabled(false);
+        }
 
     }
+
+    public boolean Camera(){
+        return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA);
+    }
+
+    public void launchCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap photo = (Bitmap) extras.get("data");
+            image.setImageBitmap(photo);
+        }
+
+    }
+
+
+
+
+
 
     public boolean onCreateOptionsMenu(Menu menu){
         MenuInflater inflater= getMenuInflater();
         inflater.inflate(R.menu.menu, menu);
-
         return true;
     }
 
@@ -70,7 +95,7 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.item2:
                 Toast.makeText(this, "Gray", Toast.LENGTH_SHORT).show();
-                Gray.to_Grey2(bit);
+
                 //GrayRS(bit);
                 return true;
 
@@ -80,12 +105,12 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.subitem4:
                 Toast.makeText(this, "Histogram Equalization Color", Toast.LENGTH_SHORT).show();
-                HistogramEqualization.colored_histogram_Equalization_Algorithm(bit);
+
                 return true;
 
             case R.id.subitem5:
                 Toast.makeText(this, "Histogram Equalization Black & White", Toast.LENGTH_SHORT).show();
-                HistogramEqualization.hea(bit);
+
                 return true;
 
             case R.id.item4:
@@ -94,47 +119,34 @@ public class MainActivity extends AppCompatActivity {
 
             case R.id.subitem6:
                 Toast.makeText(this, "Linear Dynamic ExtensionColor", Toast.LENGTH_SHORT).show();
-                LinearDynamicExtension.colored_lde(bit);
+
                 return true;
 
             case R.id.subitem7:
                 Toast.makeText(this, "Linear Dynamic Extension Black & White", Toast.LENGTH_SHORT).show();
-                LinearDynamicExtension.lde(bit);
+
                 return true;
 
             case R.id.item5:
                 Toast.makeText(this, "Reset", Toast.LENGTH_SHORT).show();
-                Coloration.reset(original, bit);
-                return true;
 
-            case R.id.item6:
-                Toast.makeText(this, "Change Image", Toast.LENGTH_SHORT).show();
                 return true;
 
 
-            case R.id.subitem8:
-                Toast.makeText(this, "Spiderman Image selected", Toast.LENGTH_SHORT).show();
-                change_toSpiderman();
-                return true;
 
-            case R.id.subitem9:
-                Toast.makeText(this, "Diagram Image selected", Toast.LENGTH_SHORT).show();
-                change_toDiagram();
-                return true;
-
-            case R.id.item7:
+           case R.id.item7:
                 Toast.makeText(this, "Other", Toast.LENGTH_SHORT).show();
                 return true;
 
 
             case R.id.subitem10:
                 Toast.makeText(this, "Colorize", Toast.LENGTH_SHORT).show();
-                Coloration.colorize(bit);
+
                 return true;
 
             case R.id.subitem11:
                 Toast.makeText(this, "Only_red", Toast.LENGTH_SHORT).show();
-                Coloration.only_red(bit);
+
                 return true;
 
 
@@ -144,28 +156,8 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    void change_toSpiderman(){
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.spidy);
-        original = bitmap.copy(bitmap.getConfig(), true);
-        bit = bitmap.copy(bitmap.getConfig(), true);
-        image.setImageBitmap(bit);
-        int N = bit.getWidth() * bit.getHeight();
-        text = findViewById(R.id.textView);
-        text.setText("Size: " + N);
 
 
-    }
-
-    private void change_toDiagram(){
-        bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.color);
-        original = bitmap.copy(bitmap.getConfig(), true);
-        bit = bitmap.copy(bitmap.getConfig(), true);
-        image.setImageBitmap(bit);
-        N = bit.getWidth() * bit.getHeight();
-        text.setText("Size: " + N);
-
-
-    }
 
 
 
